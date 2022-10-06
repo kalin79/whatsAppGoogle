14bda2157b29cf69756a0af40d4d2329443603e0
Carlos Espinoza <c.augusto.espinoza@gmail.com>
2022-10-06 13:38:13 -0500

    s

--- src/controllers/whatsappControllers.js
+++ src/controllers/whatsappControllers.js
@@ -3,19 +3,20 @@
 // - Para recibir esos mensajes que llegan desde whatsapp
 
 const verifyToken = (req, res) => {
-     try{
-          var accessToken = "ARK23442TBUIAD3244LKJFSJS34534";
-          var token = req.query["hub.verify_token"];
-          var challenge = req.query["hub.challenge"]; // es lo que debemos devolver al Whatsapp una vez que verificamos  que el token que nos da Whatsapp igual al token que hemos construido.
-          console.log(token);
-          if (challenge != null && token != null && token == accessToken){
-               res.send(challenge)
-          }else{
-               res.status(400).send();
-          }
-     }catch(e){
-          res.status(400).send();
-     }
+     // try{
+     //      var accessToken = "ARK23442TBUIAD3244LKJFSJS34534";
+     //      var token = req.query["hub.verify_token"];
+     //      var challenge = req.query["hub.challenge"]; // es lo que debemos devolver al Whatsapp una vez que verificamos  que el token que nos da Whatsapp igual al token que hemos construido.
+     //      console.log(token);
+     //      if (challenge != null && token != null && token == accessToken){
+     //           res.send(challenge)
+     //      }else{
+     //           res.status(400).send();
+     //      }
+     // }catch(e){
+     //      res.status(400).send();
+     // }
+     res.send("Hola mundo");
 }
 
 const receivedMessage = (req, res) => {
